9fd4ff92f920814a8b3d9e82a86048c3aa033203
angel <angel@host.docker.internal>
2022-03-27 22:53:05 -0300

    Substituição de imagem remota por local

--- mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ mudi/src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -15,7 +15,8 @@ public class HomeController {
 	public String home(Model model) {
 		Pedido pedido = new Pedido();
 		pedido.setNomeProduto("Xiomi Redmi Note 8");
-		pedido.setUrlImagem("https://images.kabum.com.br/produtos/fotos/sync_mirakl/192379/Usado-Xiaomi-Redmi-Note-8-64gb-Azul-Muito-Bom_1643901312_p.jpg");
+//		pedido.setUrlImagem("https://images.kabum.com.br/produtos/fotos/sync_mirakl/192379/Usado-Xiaomi-Redmi-Note-8-64gb-Azul-Muito-Bom_1643901312_p.jpg");
+		pedido.setUrlImagem("images\\Usado-Xiaomi-Redmi-Note-8-64gb-Azul.jpg");
 		pedido.setUrlProduto("https://www.kabum.com.br/produto/192379/usado-xiaomi-redmi-note-8-64gb-azul-muito-bom?gclid=CjwKCAiA4KaRBhBdEiwAZi1zztcdUkzifFMko7S50l2wAuF2stDzKx-Knyf3Pdd7JvZa2Q_mWN3fSRoCTxgQAvD_BwE");
 		pedido.setDescricao("Xiomi azul");
 		
